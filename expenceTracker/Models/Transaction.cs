3db6debb392688aa02a56dd3b2bbba4e85a43d9a
szymptom <bpodkolzin@kse.org.ua>
2024-10-05 06:13:35 +0300

    fixing bugs. WIP

--- expenceTracker/Models/Transaction.cs
+++ expenceTracker/Models/Transaction.cs
@@ -9,7 +9,6 @@ namespace expenceTracker.Models
         [Key]
         public int TransactionId { get; set; }
 
-        [Range(1, int.MinValue, ErrorMessage ="Amount should be greater than 0")]
         public int Amount { get; set; }
 
         [Column(TypeName = "nvarchar(50)")]
