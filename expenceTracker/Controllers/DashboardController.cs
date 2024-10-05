ccbb6803700d2af837c9e2d2414bbb2eb96ab704
szymptom <bpodkolzin@kse.org.ua>
2024-10-05 05:49:58 +0300

    fixed side bar overall view in all project

--- expenceTracker/Controllers/DashboardController.cs
+++ expenceTracker/Controllers/DashboardController.cs
@@ -1,11 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExpenceTracker.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExpenceTracker.Controllers
 {
-    public class DashboardController : Controller
+    public class DashboardController(TrackerContext context) : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            // past 7 days
+            var startDate = DateTime.Today.AddDays(-6);
+            var endDate = DateTime.Today;
+
+            var selectedTransactions = await context.Transactions
+                .Include(t => t.Category)
+                .Where(c => startDate <= c.Date && c.Date <= endDate)
+                .ToListAsync();
+
+            var totalIncome = selectedTransactions
+                .Where(c => c.Category.Type == "Income")
+                .Sum(t => t.Amount);
+            ViewBag.TotalIncome = totalIncome;
+
+            var totalExpense= selectedTransactions
+                .Where(c => c.Category.Type == "Expense")
+                .Sum(t => t.Amount);
+            ViewBag.TotalIncome = totalExpense;
+
+            int balance = totalIncome - totalExpense;
+            ViewBag.Balance = balance.ToString("C0");
+
+
             return View();
         }
     }
